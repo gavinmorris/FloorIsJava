1a18e3607413a20e6a4abb2483780972509bb9fd
Gavin Morris <gavin@dhcp-892b13fa.ucd.ie>
2019-02-06 10:37:27 +0000

    defined prim data types in seperate file

---
+++ src/Smells/PrimitveDataTypes.java
@@ -0,0 +1,10 @@
+package Smells;
+
+public class PrimitveDataTypes {
+	public static final String INT = "int";
+	public static final String DOUBLE = "double";
+	public static final String STRING = "String";
+	public static final String FLOAT = "float";
+	public static final String BOOL = "bool";
+	public static final String CHAR = "char";
+}
--- src/Smells/PrimitveObsession.java
+++ src/Smells/PrimitveObsession.java
@@ -1,8 +1,6 @@
 package Smells;
 
 public class PrimitveObsession {
-	private static final String INT = "int";
-	private static final String DOUBLE = "double";
-	private static final String STRING = "String";
-	private static final String FLOAT = "float";
+	
+	
 }
