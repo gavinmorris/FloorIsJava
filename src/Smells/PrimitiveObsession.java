28f65ef17ac9edc8cf336ed4bd648e8a73e40646
Gavin Morris <gavin@dhcp-892bffe1.ucd.ie>
2019-02-06 13:15:16 +0000

    the added code to primitive obsessions

--- src/Smells/PrimitiveObsession.java
+++ src/Smells/PrimitiveObsession.java
@@ -1,56 +1,69 @@
 package Smells;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class PrimitiveObsession {
 	
 	private List<String> classes;
+	private List<File> files;
 	
-	public PrimitiveObsession(List<String> classList) {
-		this.classes = classList;
+	public PrimitiveObsession(List<File> fileList) {
+		this.files = fileList;
 	}
-	
-	public int countPrimitiveTypes() {
+	public int countPrimitiveTypes() throws FileNotFoundException, IOException {
 		int numOfPts = 0;
+		for(File f: files) {
+			try(BufferedReader br = new BufferedReader(new FileReader(f))) {
+			    for(String line; (line = br.readLine()) != null; ) {
+			        numOfPts += countInt(line);
+			        numOfPts += countDouble(line);
+			        numOfPts += countFloat(line);
+			        numOfPts += countString(line);
+			        numOfPts += countBool(line);
+			        numOfPts += countChar(line);
+			    }
+			    // line is not visible here.
+			}
+		}
 		return numOfPts;
 	}
 	
+	//helper methods
 	private int countInt(String line) {
 		int countInt= 0;
 		if (line.toLowerCase().indexOf(PrimitiveDataTypes.INT.toLowerCase()) != -1 )
 			   countInt++;
 		return countInt;
-	}
-	
-	
+	}	
 	private int countDouble(String line){
 		int countDouble = 0;
 		if (line.toLowerCase().indexOf(PrimitiveDataTypes.DOUBLE.toLowerCase()) != -1 )
 			countDouble++;
 		return countDouble;
 	}
-	
 	private int countFloat(String line){
 		int countFloat = 0;
 		if (line.toLowerCase().indexOf(PrimitiveDataTypes.FLOAT.toLowerCase()) != -1 )
 			countFloat++;
 		return countFloat;
 	}
-	
 	private int countString(String line){
 		int countString = 0;
 		if (line.toLowerCase().indexOf(PrimitiveDataTypes.STRING.toLowerCase()) != -1 )
 			countString++;
 		return countString;
 	}
-	
 	private int countBool(String line){
 		int countBool = 0;
 		if (line.toLowerCase().indexOf(PrimitiveDataTypes.BOOL.toLowerCase()) != -1 )
 			countBool++;
 		return countBool;
 	}
-	
 	private int countChar(String line){
 		int countChar = 0;
 		if (line.toLowerCase().indexOf(PrimitiveDataTypes.CHAR.toLowerCase()) != -1 )
--- src/Upload/FileHandler.java
+++ src/Upload/FileHandler.java
@@ -13,13 +13,10 @@ import java.util.List;
 public class FileHandler {
 	
 	public static ArrayList<File> uploadedFiles = new ArrayList<File>();
-	
 	public static void addNewFile(Path path){
 		if(isFileTypeJava(path.toString()))
 			uploadedFiles.add(new File(path.toString()));
-		
-	}
-	
+	}	
 	public static boolean isFileTypeJava(String path) {
 		if(path.endsWith(".java"))
 			return true;
@@ -27,20 +24,12 @@ public class FileHandler {
 			return false;
 		}
 	}
-	
-	public void filesToString() {
-		for(File thisFile: uploadedFiles) {
-			try {
-				List<String> lines = Files.readAllLines(Paths.get(thisFile.getAbsolutePath()), StandardCharsets.UTF_8);
-				for(String line: lines) {
-					System.out.println(line);
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	public List<String> getClasses() {
+		List<String> classes = new ArrayList<String>();
+		for(File file: uploadedFiles) {
+			classes.add(file.getName().replace(".java", "").trim());
 		}
-		
+		return classes;
 	}
 	
 }
