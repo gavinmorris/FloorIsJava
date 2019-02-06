8e5d98e126d3a8348d56c04b07003c1909a8a296
Gavin Morris <gavin@dhcp-892b13fa.ucd.ie>
2019-02-06 10:34:50 +0000

    created first smell class -> Primitive Obsession

---
+++ src/Smells/PrimitveObsession.java
@@ -0,0 +1,8 @@
+package Smells;
+
+public class PrimitveObsession {
+	private static final String INT = "int";
+	private static final String DOUBLE = "double";
+	private static final String STRING = "String";
+	private static final String FLOAT = "float";
+}
