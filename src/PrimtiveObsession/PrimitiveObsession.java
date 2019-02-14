a46cd9d0c540f8ea6448c0503363a171612150c8
Gavin Morris <gavin1997m@gmail.com>
2019-02-14 19:55:01 +0000

    Primitve Obsessions functioning

--- src/PrimtiveObsession/PrimitiveObsession.java
+++ src/PrimtiveObsession/PrimitiveObsession.java
@@ -18,44 +18,68 @@ public class PrimitiveObsession implements PO{
     private List<String> classes = FileHandler.classes;
 
     public void report() {
-     System.out.print(primitiveCount());
+    	checkLines();
+	    System.out.println("Primitive Data Types: "+primitiveDataTypes);
+	    System.out.println("Class Objects: "+ classObjects);
+    }
+    private int primitiveDataTypes= 0;
+    private int classObjects = 0;
+    
+    public void countPDT() {
+    	primitiveDataTypes++;
+    }
+    public void countCO() {
+    	classObjects++;
     }
 
-    public int primitiveCount() {
-    	int count = 0;
+    public void checkLines() {
     	for(File f : FileHandler.uploadedFiles) {
     		try(BufferedReader br = new BufferedReader(new FileReader(f))) {
     		    for(String line; (line = br.readLine()) != null; ) {
     		        if(line.contains(PrimtiveDataTypes.BOOLEAN) && line.contains(Literals.EQUALS)) {
-    		        	count++;
+    		        	countPDT();
     		        }
     		        else if(line.contains(PrimtiveDataTypes.BYTE) && line.contains(Literals.EQUALS)) {
-    		        	count++;
+    		        	countPDT();
     		        }
     		        else if(line.contains(PrimtiveDataTypes.CHAR) && line.contains(Literals.EQUALS)) {
-    		        	count++;
+    		        	countPDT();
     		        }
     		        else if(line.contains(PrimtiveDataTypes.DOUBLE) && line.contains(Literals.EQUALS)) {
-    		        	count++;
+    		        	countPDT();
     		        }
     		        else if(line.contains(PrimtiveDataTypes.FLOAT) && line.contains(Literals.EQUALS)) {
-    		        	count++;
+    		        	countPDT();
     		        }
     		        else if(line.contains(PrimtiveDataTypes.INT) && line.contains(Literals.EQUALS)) {
-    		        	count++;
+    		        	countPDT();
     		        }
     		        else if(line.contains(PrimtiveDataTypes.LONG) && line.contains(Literals.EQUALS)) {
-    		        	count++;
+    		        	countPDT();
     		        }
     		        else if(line.contains(PrimtiveDataTypes.SHORT) && line.contains(Literals.EQUALS)) {
-    		        	count++;
+    		        	countPDT();
+    		        }
+    		        else if(classCheck(line)) {
+    		        	countCO();
     		        }
     		    }
     		} catch (IOException e) {
 				e.printStackTrace();
 			}
     	}
-    	return count;
     }
+    
+    public boolean classCheck(String line) {
+    	boolean classDefFound = false;
+    	for(String cd : classes) {
+    		if((line.contains(cd) && line.contains(Literals.EQUALS)) || line.contains(cd+".")) {
+    			classDefFound = true;
+    			break;
+    		}
+    	}
+    	return classDefFound;
+    }
+    
 
 }
