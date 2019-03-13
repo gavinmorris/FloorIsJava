e7bed9a8e4636bad2592a1e8eb084059c362cbc5
Gavin Morris <gavin1997m@gmail.com>
2019-03-13 22:28:33 +0000

    prints the class aswell

---
+++ src/General/ClassMethod.java
@@ -0,0 +1,28 @@
+package General;
+
+
+public class ClassMethod<Class, Method> {
+	private Class className;
+    private Method methodName;
+    
+    public ClassMethod(Class className, Method methodName){
+    	this.setClassName(className);
+    	this.setMethodName(methodName);
+    }
+
+	public Method getMethodName() {
+		return methodName;
+	}
+
+	public void setMethodName(Method methodName) {
+		this.methodName = methodName;
+	}
+
+	public Class getClassName() {
+		return className;
+	}
+
+	public void setClassName(Class className) {
+		this.className = className;
+	}
+}
